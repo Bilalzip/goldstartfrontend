1bd908f1e64d042513e9524212442e1650cc61f6
Umer Akram <umer.akram3344@gmail.com>
2025-05-12 14:53:21 -0400

    Console info

--- src/components/OnboardingGuard.tsx
+++ src/components/OnboardingGuard.tsx
@@ -1,24 +1,37 @@
-import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { RootState } from '@/store/store';
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/store";
 
 interface OnboardingGuardProps {
   children: React.ReactNode | ((props: { user: any }) => React.ReactNode);
 }
 
 const OnboardingGuard: React.FC<OnboardingGuardProps> = ({ children }) => {
-  const { isAuthenticated, user } = useSelector((state: RootState) => state.auth);
+  const { isAuthenticated, user } = useSelector(
+    (state: RootState) => state.auth
+  );
 
   // Avoid unnecessary re-renders and state changes
   if (!isAuthenticated) return <Navigate to="/login" replace />;
   if (!user) return null; // Wait for user data
   // Only redirect normal users (non-salespersons) who haven't completed onboarding.
-  if (!user.isSalesperson && !user.onboarding_completed && location.pathname !== '/onboarding') {
+  console.log(
+    "Salees Person: ",
+    user.isSalesperson,
+    " Onbaording: ",
+    user.onboarding_completed,
+    " pathname: ",
+    location.pathname
+  );
+  if (
+    !user.isSalesperson &&
+    !user.onboarding_completed &&
+    location.pathname !== "/onboarding"
+  ) {
     return <Navigate to="/onboarding" replace />;
   }
-  
-  
-  return <>{typeof children === 'function' ? children({ user }) : children}</>;
+
+  return <>{typeof children === "function" ? children({ user }) : children}</>;
 };
 
 export default OnboardingGuard;
